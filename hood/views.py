5db996a2214444e39b62ba625c7c7fa37995cfbc
sokkyyy <ndegwaraymond@gmail.com>
2019-11-28 17:28:53 +0300

    Added the handler for adding new users to the database if the form is valid. Also added validations for checking if email already exists in the database.

--- hood/views.py
+++ hood/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegForm
+from .models import User
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def register(request):
-    form = RegForm()
+    
 
+    if request.method == 'POST':
+        form = RegForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            full_name = form.cleaned_data['full_name']
+            password = form.cleaned_data['password']
+
+            new_user = User.objects.create_user(username,email,password)
+            new_user.full_name = full_name
+            new_user.save()
+
+            return redirect(home)
+    else:
+        form = RegForm()
     return render(request,'auth/registration.html',
     {'form':form})
