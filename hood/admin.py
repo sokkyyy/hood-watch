8fc372cbfcb60c0cdfd8944e2f8aeb38fa51253e
sokkyyy <ndegwaraymond@gmail.com>
2019-11-29 11:27:33 +0300

    Added a model for neighborhood that will store different neighborhoods in Nairobi. The model will have a default of Kibra because the app will be specifically for Kibra constituency.

--- hood/admin.py
+++ hood/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Neighborhood
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Neighborhood)
--- hood/models.py
+++ hood/models.py
@@ -6,5 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
     full_name = models.CharField(max_length=254)
-    
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100, default='Kibra')
+
+    def __str__(self):
+        return self.name    
 
