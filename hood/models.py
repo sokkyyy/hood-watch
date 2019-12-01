75d6f6145cada8fc45c8288088a667cd786199b0
sokkyyy <ndegwaraymond@gmail.com>
2019-12-01 10:11:17 +0300

    Added a posted date to store the time which hood posts were made.

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 # Create your models here.
 
     
@@ -59,7 +60,7 @@ class Post(models.Model):
     description = models.TextField()
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
     hood = models.ForeignKey(Hood,on_delete = models.CASCADE,related_name='posts')
-    # posted =
+    posted = models.DateTimeField(auto_now_add=True)  
 
     def __str__(self):
         return self.title
